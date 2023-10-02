24f9fb6d7eef3c3492988e81ebcf056c3cf745fb
Arman <ovakimian.al@phystech.edu>
2023-10-02 22:36:25 +0300

    changed names

--- assembler/assembly/assembly.cpp
+++ assembler/assembly/assembly.cpp
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <string.h>
 
-#include "assembly.h"
+#include "Assembly.h"
 #include "../Commands.h"
 
 static inline void CopyLine(FILE* inStream, FILE* outStream);
@@ -11,14 +11,6 @@ void Assembly(FILE* inStream, FILE* outStream)
     assert(inStream);
     assert(outStream);
 
-    static const char* push   = "push";
-    static const char* div    =  "div";
-    static const char* mul    =  "mul";
-    static const char* sub    =  "sub";
-    static const char* add    =  "add";
-    static const char* out    =  "out";
-    static const char* hlt    =  "hlt";
-
     static const size_t maxCommandLength = 5;
     static char command[maxCommandLength] = "";
 
@@ -26,19 +18,19 @@ void Assembly(FILE* inStream, FILE* outStream)
     {
         fscanf(inStream, "%s", command);
         
-        if (strcmp(command, push) == 0)
+        if (strcmp(command, Push) == 0)
             fprintf(outStream, "%d", (int) Commands::PUSH);
-        else if (strcmp(command, div) == 0)
+        else if (strcmp(command, Div) == 0)
             fprintf(outStream, "%d", (int) Commands::DIV);
-        else if (strcmp(command, mul) == 0)
+        else if (strcmp(command, Mul) == 0)
             fprintf(outStream, "%d", (int) Commands::MUL);
-        else if (strcmp(command, sub) == 0)
+        else if (strcmp(command, Sub) == 0)
             fprintf(outStream, "%d", (int) Commands::SUB);
-        else if (strcmp(command, add) == 0)
+        else if (strcmp(command, Add) == 0)
             fprintf(outStream, "%d", (int) Commands::ADD);
-        else if (strcmp(command, out) == 0)
+        else if (strcmp(command, Out) == 0)
             fprintf(outStream, "%d", (int) Commands::OUT);
-        else if (strcmp(command, hlt) == 0)
+        else if (strcmp(command, Hlt) == 0)
         {
             fprintf(outStream, "%d", (int) Commands::HLT);
             break;  
