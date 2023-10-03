c5df345227f90ada26cf55ffa3310c72bb3159fd
Arman <ovakimian.al@phystech.edu>
2023-10-03 14:47:33 +0300

    main processing added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main()
+#include "Processor.h"
+#include "Log.h"
+
+int main(const int argc, const char* const argv[])
 {
+    LogOpen(argv[0]);
     
-}
+    FILE* in  = nullptr;
+    if (argc > 1)
+        in = fopen(argv[1], "r");
+    else
+        in = fopen("in.txt", "r");
+
+    Processing(in);
+
+    fclose(in);
+}  
